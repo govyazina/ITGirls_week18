167075e321eff24c949a95a4c048f3cd88411ce6
Svetlana Govyazina <govyazina@gmail.com>
2022-12-16 23:26:36 +0200

    function for validation added. having some problems

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ let input = document.querySelector('#input');
 let notes = document.querySelector('.notes');
 let string = '';
 const error = document.querySelector('.error')
-
 document.addEventListener('DOMContentLoaded', generateNote)
 function addNote () {
-    // validateNote(input.value);
+    input.classList.remove('error-active')
+    if ( validateNote(input.value) === false){
+    showError()}
+    else {
     string += `<div>${input.value}</div>`;
     input.value = '';
     notes.innerHTML = string;
-    localStorage.setItem('allNotes', string)
+    localStorage.setItem('allNotes', string)}
 
 }
 
+let validateNote = (input) => {
+    return !!input;
+};
+
+function showError () {
+    input.classList.add('error-active')
+}
 function generateNote (){
     string = localStorage.getItem('allNotes');
     notes.innerHTML = string;
