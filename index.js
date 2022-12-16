2f5386f4e9a67ae2ff319a0012f261990fa6b809
Svetlana Govyazina <govyazina@gmail.com>
2022-12-16 22:40:43 +0200

    function for notes changed, some styles added

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 let input = document.querySelector('#input');
 let notes = document.querySelector('.notes');
 let string = '';
+const error = document.querySelector('.error')
 
 document.addEventListener('DOMContentLoaded', generateNote)
 function addNote () {
-    string += `${input.value} <br>`;
+    // validateNote(input.value);
+    string += `<div>${input.value}</div>`;
+    input.value = '';
     notes.innerHTML = string;
-    localStorage.setItem(notes, string)
+    localStorage.setItem('allNotes', string)
 
 }
 
 function generateNote (){
-    notes.innerHTML = localStorage.getItem(notes)
+    string = localStorage.getItem('allNotes');
+    notes.innerHTML = string;
 }
